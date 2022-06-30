b21b0b57c118331f11c4924e993f9a6fd4cae1cb
KelvinNjiraini <njirainikelvin@gmail.com>
2022-06-30 13:10:05 -0700

    101 stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints to stderr
+ *
+ * Description: Printing to the error stream
+ *
+ * Return: 1 (Error)
+ *
+ */
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
