4563fdf36794af2d57daa63807ddfae230244bfc
KelvinNjiraini <njirainikelvin@gmail.com>
2022-06-30 10:16:02 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Prints text to the console
+ *
+ * Description - prints \"Programming is like building a multilingual puzzle\"
+ */
+
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
