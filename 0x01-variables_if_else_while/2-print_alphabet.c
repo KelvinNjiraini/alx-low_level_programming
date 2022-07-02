9fc48d6f764cb212fb35ee4b188e4a2850b17fda
KelvinNjiraini <njirainikelvin@gmail.com>
2022-07-02 01:37:46 -0700

    alphabets in order in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercase
+ *
+ * Description: prints alphabet letters in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int first = 97;
+	int last = 122;
+
+	for (; first <= last; first++)
+	{
+		putchar(first);
+	}
+	putchar(10);
+
+	return (0);
+}
