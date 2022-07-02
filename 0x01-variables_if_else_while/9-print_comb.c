451c93cd218093407c8501dec51fbe5e98728307
KelvinNjiraini <njirainikelvin@gmail.com>
2022-07-02 03:31:43 -0700

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - combinations
+ *
+ * Description: working on numbers combinations
+ *
+ * Return: success (0)
+ *
+ */
+int main(0)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
