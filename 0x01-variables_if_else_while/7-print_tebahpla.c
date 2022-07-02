53bb61637de986b3620be1386848d374a1633e62
KelvinNjiraini <njirainikelvin@gmail.com>
2022-07-02 02:49:34 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - print letters
+ *
+ * Description: alphabet in reverse
+ *
+ * Return: Success (0)
+ *
+ */
+int main(void)
+{
+	int first = 122;
+	int last = 97;
+
+	for (; first >= last; first--)
+	{
+		putchar(first);
+	}
+	putchar(10);
+
+	return (0);
+}
