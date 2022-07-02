528867ee4855c313dbd8e1758395b6d4a1726925
KelvinNjiraini <njirainikelvin@gmail.com>
2022-07-02 02:30:09 -0700

    printing numbers take 2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing out digits from 0 to 9
+ *
+ * Description: Printing numbers
+ *
+ * Return: always 0 for success
+ *
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+}
